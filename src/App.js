fd64cf3ddae6091ef7225ba1fa7ba7f4613624a7
Yashh918 <yashdhopeshwarkar@gmail.com>
2024-04-05 10:59:38 +0530

    added react router, can fetch news category wise

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react'
-import { Navbar } from './components/Navbar'
-import { News } from './components/News'
+import React, { Component } from "react";
+import { Navbar } from "./components/Navbar";
+import { News } from "./components/News";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 export default class App extends Component {
-
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize={6} country="in" category="health"/>
+        <Router>
+          <Navbar />
+
+         <Routes>
+         <Route path="/" element={ <News pageSize={6} key="a" country="in" category="general" />}/>
+          <Route path="/business" element={ <News pageSize={6} key="b" country="in" category="business" />}/>
+          <Route path="/entertainment" element={ <News pageSize={6} key="c" country="in" category="entertainment" />}/>
+          <Route path="/health" element={ <News pageSize={6} key="d" country="in" category="health" />}/>
+          <Route path="/science" element={ <News pageSize={6} key="e" country="in" category="science" />}/>
+          <Route path="/sports" element={ <News pageSize={6} key="f" country="in" category="sports" />}/>
+          <Route path="/technology" element={ <News pageSize={6} key="g" country="in" category="technology" />}/>
+         </Routes>
+         
+        </Router>
       </div>
-    )
+    );
   }
 }
-
-
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,11 +5,11 @@ export class NewsItem extends Component {
         let { title, description, imageUrl, newsUrl } = this.props;
         return (
             <div className="card h-100">
-                <img src={imageUrl ? imageUrl : "https://cdn4.iconfinder.com/data/icons/picture-sharing-sites/32/No_Image-1024.png"} className="card-img-top" alt="..." />
+                <img src={imageUrl ? imageUrl : "https://th.bing.com/th/id/OIP.0dzPl0BUoR9sKlirIPvhBAHaEb?w=322&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7"} className="card-img-top" alt="..." />
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary mt-auto">Read more</a>
+                    <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark mt-auto">Read more</a>
                 </div>
             </div>
         )
